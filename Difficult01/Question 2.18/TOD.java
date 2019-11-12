f44689f3d21478c49331fd9013c56b1b256ccace
ndzib <brunoserkwi@gmail.com>
2019-11-12 20:56:30 +0100

    solved question 2.18 closes #6

---
+++ Difficult01/Question 2.18/TOD.java
@@ -0,0 +1,8 @@
+//print the word: "TOD" using asterisks
+public class TOD {
+
+	public static void main(String[] args) {
+		String tod = "*** *** **\n *  * * * *\n *  *** **\n";
+		System.out.println(tod);
+	}
+}
