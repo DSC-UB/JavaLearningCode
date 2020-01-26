4dddfefc889f469f4b1068ece1ec0b38525cfaf9
math-alpha <yopasylvestre@gmail.com>
2020-01-26 17:28:02 +0100

    added some styling

--- src/Multiples.java
+++ src/Multiples.java
@@ -4,17 +4,17 @@ class Multiples{
     public static void main(String[] args) {
         System.out.print("Number: ");
         for (int i = 0; i <= 10; i++) {
-            System.out.print(i);
+            System.out.print(i + ", ");
         }
 
         System.out.print("Doubles: ");
         for (int i = 0; i <= 10; i++) {
-            System.out.print(i * 2);
+            System.out.print(i * 2 + ", ");
         }
 
         System.out.print("Triples: ");
         for (int i = 0; i <= 10; i++) {
-            System.out.print(i * 3);
+            System.out.print(i * 3 + ", ");
         }
     }
 }
